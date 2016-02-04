61f62da48cc5d8853096f746a52e4c198f38dbe9
Andy Perry <andyjp94@gmail.com>
2016-02-04 09:29:29 +0000

    added bats as a submodule and moved the bats scripts to bats_scripts

--- scripts/v3/ires/setup.sh
+++ scripts/v3/ires/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARRAY=(./bats/icommands.bats ./setup_3/ires_resource.bats)
+ARRAY=(./bats_scripts/icommands.bats ./setup_3/ires_resource.bats)
 
 for FILE in ${ARRAY[@]};do
         echo $FILE
