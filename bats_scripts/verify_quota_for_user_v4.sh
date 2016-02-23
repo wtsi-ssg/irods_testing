0bbc51c954ff977c7781ff2e8d5f09a6fcd5b7d0
John Constable <john.constable@sanger.ac.uk>
2016-02-23 10:04:50 +0000

    New V4 JSON files both for post upgrade V4 and new install V4, modifying  V4 tests to use the same.

---
+++ bats_scripts/verify_quota_for_user_v4.sh
@@ -0,0 +1,22 @@
+#test that quotas work for a single user
+# needs to have the following run first
+# quota_setup.bats
+# setup_quotatestaccount2_authentication.bats
+
+setup() {
+    export irodsEnvFile='/home/vagrant/.irods/irods_environment.json_quotatestaccount2'
+    export irodsAuthFileName='/home/vagrant/.irods/.irodsA_quotatestaccount2'
+    dd if=/dev/zero of=/tmp/quotatestaccount2_test bs=1b count=101
+}
+
+@test "Confirm Failure to upload file larger than quota for quotatestaccount2" {
+    run iput -K /tmp/quotatestaccount2_test
+    [ "$status" -eq 3 ]
+}
+
+teardown() {
+    unset irodsEnvFile
+    unset irodsAuthFileName
+    rm /tmp/quotatestaccount2_test
+}
+
