157128e246e82d7638124ed83cb795f47a33f610
Andy Perry <ap21@sanger.ac.uk>
2016-02-23 15:26:48 +0000

    changed the ssh user to irods

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
 
-client.connect("192.168.50.11", username="vagrant",password='vagrant')
+client.connect("192.168.50.11", username="irods",password='y')
 
-stdin, stdout, stderr = client.exec_command('scripts/v4/icat/setup')
+#stdin, stdout, stderr = client.exec_command('scripts/v4/icat/setup')
+stdin, stdout, stderr = client.exec_command('echo hello')
 
 print "stderr: ", stderr.readlines()
 print "pwd: ", stdout.readlines()
